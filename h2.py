248b090e24d2e80c04322ffc73a6bf3e3d98f461
harshv2013 <harsh2013@gmail.com>
2018-06-28 11:15:29 +0530

    First commit

--- h2.py
+++ h2.py
@@ -1,2 +1,4 @@
 '''This is second file we are making'''
+'''These are chages again'''
+'''ya be cool''
 print("Hello Git2")
---
+++ h3.py
@@ -0,0 +1,2 @@
+'''This is random third file we are making'''
+print("Hello Git3")
